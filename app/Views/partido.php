b8ca6e06e1f36e867456f04ea6d6f3f8be15b16f
atejedac <alfonsotejeda0213@gmail.com>
2021-10-28 20:19:38 -0600

    Codigo|
    Eliminar tituos

---
+++ app/Views/partido.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+
+	<title >Partidos</title>
+
+	<title>Partidos</title>
+
+     
+	<!-- Link hacia el archivo de estilos css -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/partido.css">
+</head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+	<body>
+	</br>
+	</br>
+	</br>
+		<img class="pelota" src="2.png" width="250" height="125">
+		<div id="padre">
+			<table cellspacing="0">
+				<caption>Partidos</caption>
+				<tr>
+					<th>Partido</th>
+					<th>Fecha</th>
+					<th>Status</th>
+				</tr>
+				<tr>
+					<td>Partido 1</td>
+					<td>Fecha 1</td>
+					<td>Status 1</td>
+				</tr>
+				<tr>
+					<td>Partido 1</td>
+					<td>Fecha 1</td>
+					<td>Status 1</td>
+				</tr>
+				<tr>
+					<td>Partido 1</td>
+					<td>Fecha 1</td>
+					<td>Status 1</td>
+				</tr>
+				<tr>
+					<td>Partido 1</td>
+					<td>Fecha 1</td>
+					<td>Status 1</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
